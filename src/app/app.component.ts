7573fd1ecec2da3a38dbd98589d43471f3917fc6
Jakub Jakubiak <34916730+JakubJakubiak@users.noreply.github.com>
2023-11-04 00:01:56 +0100

    ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.SCSS']
 })
 export class AppComponent {
   title = 'js-tasks';
+  items: string[] = [`  factorial(n: number): number {
+    if (n == 0 || n == 1) {
+      return 1;
+    } else {
+      return n * this.factorial(n - 1);
+    }
+  }
+}`,
+    
+    `function factorial(n) {
+    return Array.from({length: n},
+       (_, i) => i + 1).reduce((a, b) => a * b, 1);
+}` ,
+];
+
+
+    inputNumber: number | undefined;
+    factorialResult: number | undefined;
+
+  factorial(n: number): number {
+    if (n == 0 || n == 1) {
+      return 1;
+    } else {
+      return n * this.factorial(n - 1);
+    }
+  }
+
+  calculateFactorial() {
+    if (this.inputNumber) {
+      this.factorialResult = this.factorial(this.inputNumber);
+    }
+  }
+
 }
+
+
