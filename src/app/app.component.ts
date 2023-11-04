d8caf8364b942777f54223be0607f153dbea4fa7
Jakub Jakubiak <34916730+JakubJakubiak@users.noreply.github.com>
2023-11-04 23:24:33 +0100

    scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.SCSS']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'js-tasks';
